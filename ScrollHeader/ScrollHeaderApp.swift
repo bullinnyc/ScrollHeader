e38db00b43b6c87d7cee7b716fc7c65b45682b16
bullinnyc <i8u11@icloud.com>
2024-11-16 12:53:12 +0700

    Initial Commit

---
+++ ScrollHeader/ScrollHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollHeaderApp.swift
+//  ScrollHeader
+//
+//  Created by Dmitry Kononchuk on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrollHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
